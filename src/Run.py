dd5ab964f32697e4a8104c84a0b021af31ca99cc
Stefan Geyer <stefangeyer93@googlemail.com>
2017-07-17 18:43:39 +0200

    Improvements to fit ex3 tasks

--- src/Run.py
+++ src/Run.py
@@ -24,7 +24,7 @@ def main():
                                                         data.validationSet,
                                                         data.testSet,
                                                         learningRate=0.1,
-                                                        epochs=10)
+                                                        epochs=200)
 
     # Train the classifiers
     print("=========================")
